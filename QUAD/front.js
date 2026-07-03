f509e23dc68e75b993c80f0798678cb9e0d6a351
iki1uc <entertrainer@web.de>
2026-07-03 21:18:01 +0200

    Update front.js

--- QUAD/front.js
+++ QUAD/front.js
@@ -1,1 +1,10 @@
-1
+export function QUAD_FRONT(msg = "") {
+    return {
+        quad: "front",
+        view: "render",
+        input: String(msg),
+        active: true,
+        cube: 81,
+        layer: 1
+    };
+}
